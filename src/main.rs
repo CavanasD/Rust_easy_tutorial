7960b948c392a59c9faf1e2bb7d085e4126e6799
CavanasD <m18404288789@163.com>
2026-07-15 14:57:49 +0800

    Function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-const MAX_CAT_GIRL: u8 = 10; // 必须显式声明Data Type
-static CAT_GIRL_NAME: &str = "N1n3Bird";
-static mut CAT_GIRL_NAME_1: &str = "N1n3Bird";//unsafe
+
 fn main(){
-    let a = MAX_CAT_GIRL;
-    let b = 10;
-    let c  = CAT_GIRL_NAME;
+    println!("{}", transfer_to_meow(1));
+}
+
+fn transfer_to_meow(x: i32) -> i32 {
+    println!("You are meowed {}", x);
+    let y = x + 114514;
+    y
 }
 
