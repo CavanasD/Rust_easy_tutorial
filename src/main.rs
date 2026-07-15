f341600bc57813da40c68c5a8aeccf150ae12cee
CavanasD <m18404288789@163.com>
2026-07-15 12:08:41 +0800

    变量

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
 fn main(){
-    println!("hello world");
+    //creation
+    let a = 10;
+    let a1: i16 = 1;
+    //mutability
+    let mut b = 10;
+    b = 100;
+
+    //shadowing
+    let c = 10;
+    let c = 20;
+
+    println!("{c}");
+
+    //scope
+    let d = 30;
+    {
+        let d = 40;
+        println!("{d}");
+
+    }
+    {
+        let e = 100;
+    }
+    println!("{e}");
+    println!("{d}");
+
+
 
 }
